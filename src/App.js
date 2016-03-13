4d197616a3cd86335e917014f0cd071ba866ee60
Marshal <marshal.wu@qq.com>
2016-03-13 10:45:42 +0800

    init commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
 import React, { Component } from 'react';
-import { NICE, SUPER_NICE } from './colors';
+import RaisedButton from 'material-ui/lib/raised-button';
 
-class Counter extends Component {
+class Helloworld extends Component {
   constructor(props) {
     super(props);
-    this.state = { counter: 0 };
-    this.interval = setInterval(() => this.tick(), 1000);
-  }
-
-  tick() {
-    this.setState({
-      counter: this.state.counter + this.props.increment
-    });
   }
 
   componentWillUnmount() {
@@ -20,9 +12,7 @@ class Counter extends Component {
 
   render() {
     return (
-      <h1 style={{ color: this.props.color }}>
-        Counter ({this.props.increment}): {this.state.counter}
-      </h1>
+      <RaisedButton label="设置" />
     );
   }
 }
@@ -31,8 +21,7 @@ export class App extends Component {
   render() {
     return (
       <div>
-        <Counter increment={1} color={NICE} />
-        <Counter increment={5} color={SUPER_NICE} />
+        <Helloworld />
       </div>
     );
   }
